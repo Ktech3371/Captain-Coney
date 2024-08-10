81af18080f74af640e9f3fa48f15451b548fea5c
Kurt Miller <captainconey24@gmail.com>
2024-08-10 12:58:50 -0400

    Converted website to react app.

---
+++ reactdev/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <html lang="en">
+      <body>
+        <div style={{ textAlign: "center" }}>
+
+          <h1>Captain Coney</h1>
+          <p>Est. 2024</p>
+          <h2>
+            <img src="/images/CCLogo.jpg" alt="description" />
+          </h2>
+          <h2><b>Coming Soon!!!</b></h2>
+        </div>
+
+      </body>
+    </html>
+  );
+}
+
+export default App;
